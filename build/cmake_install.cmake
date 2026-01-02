a6e6e238d78ceadb1f239a2a836db46167f6be24
Brunocor26 <bruno.correia@ubi.pt>
2026-01-02 17:10:17 +0000

    Final

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/bruno/Desktop/ProjetoFinal_CG_Maze
+# Install script for directory: /home/bruno/Desktop/3Ano/CG/ProjetoFinal
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/bruno/Desktop/ProjetoFinal_CG_Maze/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/bruno/Desktop/3Ano/CG/ProjetoFinal/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
